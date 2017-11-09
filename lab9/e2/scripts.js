3524b3a308a36d5eb5e5679aca4f3ecd01632f51
TylerJBourg <31972335+TylerJBourg@users.noreply.github.com>
2017-11-09 17:34:50 -0500

    oops

--- lab9/e2/scripts.js
+++ lab9/e2/scripts.js
@@ -1,5 +1,17 @@
-//A Function to printout Hurrah! when called
+//Sets the initial colours of the three elements on page load
+document.getElementById("blue").style.color="blue";
+document.getElementById("green").style.color="green";
+document.getElementById("red").style.color="red";
 
-function pushMe(){
-	document.getElementById("output").innerHTML="Hurrah!";
+//A function to change colours of elements when called
+function mix(){
+
+	var first = "#0C0CC8";
+	var second = "#00CC00";
+	var third = "#FF0000";
+
+	document.getElementById("blue").style.color=first;
+	document.getElementById("green").style.color=second;
+	document.getElementById("red").style.color=third;
+
 }
